de1d027eed2acd735613386150aa16c8335e7017
s.t.o.k.a.t.o <s.t.o.k.a.t.p@gmail.com>
2016-09-29 23:17:37 +0300

    Аргументы

--- Assets/Scripts/Example_Pseudocode.cs
+++ Assets/Scripts/Example_Pseudocode.cs
@@ -3,6 +3,32 @@ using System.Collections;
 
 public class Example_Pseudocode : MonoBehaviour {
 
+    public Vector3 pos = Vector3.zero;
+    public float speed = 0.2f;
+
+    Vector3 Movement (float dist)
+    {
+        Vector3 vec = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            vec.x -= dist;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            vec.x += dist;
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            vec.z += dist;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            vec.z -= dist;
+        }
+        return vec;
+    }
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +36,6 @@ public class Example_Pseudocode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.position += Movement(speed);
 	}
 }
